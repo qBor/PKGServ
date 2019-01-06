2878a27e5129d9bbd375f7bac4d354bc679ba28c
kevxxf <kevin.fosse057@gmail.com>
2019-01-06 16:23:58 +0100

    added a PKG folder example, folder button now work, you can't start server without selecting a folder, added a label to show the path of the selected folder

--- PKGServ/ViewController.swift
+++ PKGServ/ViewController.swift
@@ -15,34 +15,74 @@ import Swifter
 
 
 class ViewController: NSViewController {
-    
-    
+
+
+    let fm = FileManager.default
+    let path = Bundle.main.resourcePath!
     let server = HttpServer()
     var localIP :String = ""
     var port :String = "80"
     var serverStarted = true
-    
+    var selectedPath = ""
     
     @IBOutlet weak var makeListforHan: NSButton!
     @IBOutlet weak var serverLblStat: NSTextField!
     @IBOutlet weak var localIPTextField: NSTextField!
     @IBOutlet weak var portTextField: NSTextField!
     @IBOutlet weak var test: NSTextField!
-
-
-
+    @IBOutlet weak var selectedPathLabel: NSTextField!
     
+    func openPanel() {
+        let openPanel = NSOpenPanel()
+        openPanel.canChooseFiles = false
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = true
+        openPanel.canCreateDirectories = false
+        openPanel.title = "Select PKG folder"
+        
+        openPanel.beginSheetModal(for:self.view.window!) { (response) in
+            if response.rawValue == NSFileHandlingPanelOKButton {
+                self.selectedPath = openPanel.url!.path
+                // do whatever you what with the file path
+                self.selectedPathLabel.stringValue = self.selectedPath
+            }
+            openPanel.close()
+            
+        }
+    }
+
+    @IBAction func openFolder(_ sender: NSButton) {
+        openPanel()
+    }
+    
+ 
+    
+    // Open Network preference
     @IBAction func openNetworkBtn(_ sender: Any) {NSWorkspace.shared.open(URL(fileURLWithPath: "/System/Library/PreferencePanes/Network.prefPane"))}
+    
+    // open kevxxf Git
     @IBAction func openGitHub(_ sender: NSButton) {NSWorkspace.shared.open(URL(string: "https://github.com/qBor/PKGServ")!)}
     
-@IBAction func fivepxGit(_ sender: Any) {NSWorkspace.shared.open(URL(string: "https://github.com/FivePixels")!)}
+    // open 5PX Git
+    @IBAction func fivepxGit(_ sender: Any) {NSWorkspace.shared.open(URL(string: "https://github.com/FivePixels")!)}
+    
+    // Button to start/stop server
     @IBAction func whenSrvBtnPressed(_ sender: NSButton) {
          let portInt = UInt16(port)
         
         if serverStarted == true {
             
-            
-            
+            if(selectedPath == "") {
+                let alert = NSAlert()
+                alert.alertStyle = .critical
+                alert.messageText = "No selected folder"
+                alert.informativeText = "Before starting the server, you must choose the folder containing the PKG."
+                alert.runModal()
+                openPanel()
+                
+            }
+            else {
+            server["/PKG/:path"] = shareFilesFromDirectory(selectedPath)
             server["/"] = scopes {
                 html {
                     body {
@@ -67,24 +107,20 @@ class ViewController: NSViewController {
             } catch {
                 print("Server start error: \(error)")
             }
+                // -----
+                sender.title = "Stop server"
+                serverLblStat.textColor = NSColor.green
+                serverLblStat.stringValue = "Server running on port \(port)"
+                serverStarted = !serverStarted;
+            }
 
             
-            // -----
-            sender.title = "Stop server"
-            serverLblStat.textColor = NSColor.green
-            serverLblStat.stringValue = "Server running on port \(port)"
-            serverStarted = !serverStarted;
+         
         }
         
         else {
             
-            do {
-                try server.stop()
-                print("Server has stopped.")
-                
-            } catch {
-                print("Server stop error: \(error)")
-            }
+            server.stop()
             
 
             // -----------
