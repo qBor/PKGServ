e79b70cc26ad3f031ba0fc391287b76a0d95c7c6
kevxxf <kevin.fosse057@gmail.com>
2019-01-07 21:37:18 +0100

    Push for FivePixel change

---
+++ PKGServ/Header.h
@@ -0,0 +1,10 @@
+//
+//  Header.h
+//  PKGServ
+//
+//  Created by Kevin Fosse on 07/01/2019.
+//  Copyright © 2019 Kevin Fosse. All rights reserved.
+//
+
+#include<ifaddrs.h>
+
--- PKGServ/ViewController.swift
+++ PKGServ/ViewController.swift
@@ -14,6 +14,7 @@ import Cocoa
 import Swifter
 
 
+
 class ViewController: NSViewController {
 
 
@@ -34,6 +35,43 @@ class ViewController: NSViewController {
     @IBOutlet weak var portTextField: NSTextField!
     @IBOutlet weak var test: NSTextField!
     @IBOutlet weak var selectedPathLabel: NSTextField!
+    
+    
+    func getWiFiAddress() -> String? {
+        var address : String?
+        
+        // Get list of all interfaces on the local machine:
+        var ifaddr : UnsafeMutablePointer<ifaddrs>?
+        guard getifaddrs(&ifaddr) == 0 else { return nil }
+        guard let firstAddr = ifaddr else { return nil }
+        
+        // For each interface ...
+        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let interface = ifptr.pointee
+            
+            // Check for IPv4 or IPv6 interface:
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            //if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {  // **ipv6 committed
+            if addrFamily == UInt8(AF_INET){
+                
+                // Check interface name:
+                let name = String(cString: interface.ifa_name)
+                if  name == "en0" {
+                    
+                    // Convert interface address to a human readable string:
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                                &hostname, socklen_t(hostname.count),
+                                nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostname)
+                }
+            }
+        }
+        freeifaddrs(ifaddr)
+        
+        return address
+    }
+
     
     func presentFolderSelection() {
         let panel = NSOpenPanel()
@@ -171,7 +209,13 @@ class ViewController: NSViewController {
         super.viewDidLoad()
        
         // Do any additional setup after loading the view.
-        
+        if let addr = getWiFiAddress() {
+            print(addr)
+            localIPTextField.stringValue = addr
+        } else {
+            print("No WiFi address")
+        }
+
         
         // ...
         
